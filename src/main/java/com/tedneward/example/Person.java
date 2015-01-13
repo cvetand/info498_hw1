b7d0e0379307a6a06070e9922f118daebeeea35f
Cvetan Damyanov <cvetand@Cvetan-Damyanovs-MacBook-Pro-2.local>
2015-01-13 00:29:41 -0800

    Finished

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -3,11 +3,12 @@ package com.tedneward.example;
 import java.beans.*;
 import java.util.*;
 
-public class Person {
+public class Person implements Comparable<Person>
+{
   private int age;
   private String name;
   private double salary;
-  private String ssn;
+  private String ssn = "";
   private boolean propertyChangeFired = false;
   
   public Person() {
@@ -42,6 +43,26 @@ public class Person {
     this.pcs.firePropertyChange("ssn", old, value);
     propertyChangeFired = true;
   }
+
+  public void setAge(int age) {
+    if (age<0) {
+      throw new IllegalArgumentException("Age cannot be less than 0");
+    }
+    this.age = age;
+  }
+
+  public void setName(String name) {
+    if (name==null){
+      throw new IllegalArgumentException("Name cannot be null");
+    }
+    this.name = name;
+
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
   public boolean getPropertyChangeFired() {
     return propertyChangeFired;
   }
@@ -57,13 +78,44 @@ public class Person {
   public int timeWarp() {
     return age + 10;
   }
-  
-  public boolean equals(Person other) {
-    return (this.name.equals(p.name) && this.age == p.age);
+
+  //@Override
+  public boolean equals(Object other) {
+    if (other instanceof Person) {
+      Person p = (Person)other;
+      return p.name == this.name && p.age == this.age;
+    }
+    return false;
   }
 
-  public String tostring() {
-    return "{{FIXME}}";
+  public static class AgeComparator implements Comparator<Person>{
+    public int compare(Person first, Person second) {
+      return first.age - second.age;
+    }
+  }
+ 
+  //@Override
+  public int compareTo(Person other){
+    double diff = this.salary - other.salary;
+    if (diff > 0)
+      return -1;
+    else if (diff < 0)
+      return 1;
+    else 
+      return 0;
+  }
+
+  public String toString() {
+    return "[Person name:"+name+" age:"+age+" salary:"+salary+"]";
+  }
+
+  public static ArrayList<Person> getNewardFamily(){
+    ArrayList<Person> NewardFam = new ArrayList<Person>();
+    NewardFam.add(new Person("Ted", 41, 250000.0));
+    NewardFam.add(new Person("Charlotte", 43, 150000.0));
+    NewardFam.add(new Person("Michael", 22, 10000.0));
+    NewardFam.add(new Person("Matthew", 15, 0.0));
+    return NewardFam;
   }
 
   // PropertyChangeListener support; you shouldn't need to change any of
